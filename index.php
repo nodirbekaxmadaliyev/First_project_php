870afc7d6ad3d5f6222f1e3437e85c65f623d3a2
nodirbekaxmadaliyev <153220082+nodirbekaxmadaliyev@users.noreply.github.com>
2023-12-08 19:50:48 +0500

    Add files via upload
    Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
    
    Symbol       Value
    I             1
    V             5
    X             10
    L             50
    C             100
    D             500
    M             1000
    For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
    
    Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
    
    I can be placed before V (5) and X (10) to make 4 and 9. 
    X can be placed before L (50) and C (100) to make 40 and 90. 
    C can be placed before D (500) and M (1000) to make 400 and 900.
    Given a roman numeral, convert it to an integer.

--- index.php
+++ index.php
@@ -1,9 +1,67 @@
 <?php
-$nums = [15, 85, 15];
-$target = 100;
-echo "Assalomu alaykum<br>";
-    for ($i = 0; $i < count($nums) - 1; $i++ )
-        for ($f = $i + 1; $f < count($nums); $f ++)
-            if ($nums[$i] + $nums[$f] === $target)
-                echo "[$i, $f]";
+//13-masaqla Roman to Integer
+class Solution {
+
+    /**
+     * @param String $s
+     * @return Integer
+     */
+    function check($s0, $s1) : bool
+    {
+        if ($s0==='I' && ($s1 ==='V' || $s1 === 'X'))   return true;
+        if ($s0==='X' && ($s1 ==='L' || $s1 === 'C'))
+            return true;
+        if ($s0==='C' && ($s1 ==='D' || $s1 === 'M'))
+            return true;
+        return false;
+    }
+
+    function sum($z) : int
+    {
+        switch($z)
+        {
+            case "I": return 1;
+            case "V": return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+        }
+        return 0;
+    }
+
+    function sum1($z) : int
+    {
+        switch($z)
+        {
+            case "V": return 4;
+            case 'X': return 9;
+            case 'L': return 40;
+            case 'C': return 90;
+            case 'D': return 400;
+            case 'M': return 900;
+        }
+        return 0;
+    }
+
+    function romanToInt($s)
+    {
+
+
+        $summ = 0;
+        $i = 0;
+        while ($i < strlen($s))
+        {
+            if ($i < strlen($s) - 1 && $this->check($s[$i], $s[$i+1]) == 1)
+            {
+                $summ += $this->sum1($s[$i+1]);
+                $i ++;
+            }
+            else $summ +=$this->sum($s[$i]);
+            $i ++;
+        }
+        return $summ;
+    }
+}
 ?>
